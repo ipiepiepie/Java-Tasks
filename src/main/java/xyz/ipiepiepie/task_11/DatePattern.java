4a29f8668de0928fff78e8a7f1a6abaae82cee94
ipiepiepie <pie@mac.local>
2023-12-31 00:45:33 +0300

    :heavy_plus_sign: Add 11 task

---
+++ src/main/java/xyz/ipiepiepie/task_11/DatePattern.java
@@ -0,0 +1,68 @@
+package xyz.ipiepiepie.task_11;
+
+import java.util.regex.Pattern;
+
+public class DatePattern {
+    // day //
+    private static final String day = "([1-9]|0[1-9]|[12][0-9]|3[01])";
+    // month //
+    private static final String month = "(0[1-9]|1[012])";
+    private static final String[] months = {
+            "декабрь", "январь", "февраль",
+            "март", "апрель", "май",
+            "июнь", "июль", "август",
+            "сентябрь", "октябрь", "ноябрь"
+    };
+    // year //
+    private static final String year = "(\\d{1,4})";
+    // divider //
+    private static final String point = "\\.";
+    private static final String space = " ";
+
+    /// PATTERNS ///
+
+    /**
+     * Get 'DD.MM.YYYY' {@link Pattern}
+     * @return {@link Pattern}
+     */
+    public static Pattern getDateMonthNumberYearPattern() {
+        return Pattern.compile(day + point + month + point + year, Pattern.CASE_INSENSITIVE);
+    }
+
+    /**
+     * Get 'DD Month YYYY' {@link Pattern}
+     * @return {@link Pattern}
+     */
+    public static Pattern getDateMonthNameYearPattern() {
+        return Pattern.compile(day + space + getMonthsPattern() + space + year, Pattern.CASE_INSENSITIVE);
+    }
+
+    /**
+     * Get 'DD Month' {@link Pattern}
+     * @return {@link Pattern}
+     */
+    public static Pattern getDateMonthNamePattern() {
+        return Pattern.compile(day + space + getMonthsPattern(), Pattern.CASE_INSENSITIVE);
+    }
+
+    /// MONTHS ///
+
+    /**
+     * Get month names pattern
+     *
+     * @return months pattern
+     */
+    private static String getMonthsPattern() {
+        StringBuilder pattern = new StringBuilder();
+
+        // build pattern from known months
+        for (String month : months) {
+            pattern.append(month);
+            pattern.append("|");
+        }
+
+        // return pattern
+        return String.format("(%s)", pattern.substring(0, pattern.length() - 1));
+    }
+
+}
---
+++ src/main/java/xyz/ipiepiepie/task_11/Main.java
@@ -0,0 +1,146 @@
+package xyz.ipiepiepie.task_11;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import xyz.ipiepiepie.task_10.Candy;
+import xyz.ipiepiepie.task_10.gui.Form;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+/**
+ * Finds {@link Date Dates} in text
+ * <p>
+ * Task №6 from <a href="https://edu.vsu.ru/pluginfile.php/6688971/mod_folder/content/0/Task_11.doc?forcedownload=1">this list</a>
+ */
+public class Main {
+
+    /**
+     * Entry point.
+     */
+    public static void main(String[] args) {
+        Options options = new Options();
+        // register options
+        options.addOption("d", "debug", false, "Run program in debug mode");
+        options.addOption("g", "gui", false, "Run program in GUI mode");
+        options.addOption("m", "money", true, "Specify money amount");
+
+        // parse command
+        parseArgs(options, args);
+    }
+
+    /// COMMAND FUNCTIONS ///
+
+    /**
+     * Parses jar args.
+     *
+     * @param options registered arguments.
+     * @param args received arguments.
+     */
+    private static void parseArgs(Options options, String[] args) {
+        try {
+            CommandLine cmd = new DefaultParser().parse(options, args);
+
+            if (cmd.hasOption("d")) {
+                String path = "src/main/resources/task_11/debug_%d.txt";
+
+                // iterate over tests
+                for (int i = 0; i < 5; i++) {
+                    System.out.printf("======== Testing debug_%d.txt ========%n", i);
+                    String text = read(Path.of(String.format(path, i)));
+
+                    // skip text if it's empty
+                    if (text == null || text.isEmpty()) continue;
+
+                    // print given text
+                    System.out.println("Current text:");
+                    System.out.println(text);
+                    // print fetched Dates
+                    System.out.println("Fetched dates:");
+                    System.out.println(findDates(text));
+                }
+            } else if (cmd.hasOption("g")) {
+                new Form();
+            } else {
+                String path = cmd.getArgs()[0];
+                String text = read(Path.of(path));
+
+                // skip text if it's empty
+                if (text == null || text.isEmpty()) {
+                    System.err.println("Can't read text from selected file");
+                    return;
+                }
+
+                // print given text
+                System.out.println("Current text:");
+                System.out.println(text);
+                // print fetched Dates
+                System.out.println("Fetched dates:");
+                System.out.println(findDates(text));
+            }
+        } catch (ParseException e) {
+            System.err.println("Unexpected exception:" + e.getMessage());
+        }
+    }
+
+    /// DATE PARSERS ///
+
+    /**
+     * Fetch {@link Date Dates} from text.
+     *
+     * @param text processing text
+     * @return {@link List} of {@link Date Dates}
+     */
+    private static List<String> findDates(String text) {
+        List<String> dates = new ArrayList<>();
+
+        // parse DD.MM.YYYY
+        dates.addAll(findDates(text, DatePattern.getDateMonthNumberYearPattern()));
+        // parse DD Month YYYY
+        dates.addAll(findDates(text, DatePattern.getDateMonthNameYearPattern()));
+        // parse DD Month
+        dates.addAll(findDates(text, DatePattern.getDateMonthNamePattern()));
+
+        return dates;
+    }
+
+    /**
+     * Find {@link Date Dates}, matching selected {@link Pattern}.
+     *
+     * @param text text to look for {@link Date Dates}.
+     * @param pattern {@link Date} {@link Pattern}.
+     *
+     * @return {@link List} of {@link Date Dates} from text
+     */
+    private static List<String> findDates(String text, Pattern pattern) {
+        return pattern.matcher(text.toLowerCase()).results().map(MatchResult::group).toList();
+    }
+
+    /// I/O FUNCTIONS ///
+
+    /**
+     * Read {@code text} from {@link File}.
+     *
+     * @param path {@link Path} to {@code text}.
+     *
+     * @return {@code text} if exists, otherwise {@code null}
+     */
+    public static String read(Path path) {
+        try {
+            return Files.readString(path);
+        } catch (IOException e) {
+            System.out.println("File not found: " + e);
+        }
+
+        return null;
+    }
+
+}
